0d117e140aa40a97fb8a10e911587be06771f5b3
Sean Dickinson <contact@seandickinson.dev>
2022-12-02 08:40:32 -0500

    refactor: use enumerable

--- 01.rb
+++ 01.rb
@@ -1,20 +1,39 @@
 module Day01
+  class GroupedList
+    include Enumerable
+    def initialize(raw_input)
+      @raw_input = raw_input
+    end
+
+    def each
+      # See method here https://ruby-doc.org/3.1.3/Enumerable.html#method-i-chunk_while
+      @raw_input.chunk_while { |_line, next_line| next_line != "" }
+        .each do |list|
+          yield list.reject(&:empty?)
+        end
+    end
+  end
+
   class Items
     class << self
       def from(raw_text_list)
-        # See method here https://ruby-doc.org/3.1.3/Enumerable.html#method-i-chunk_while
-        raw_text_list.chunk_while { |line, next_line| next_line != "" }
-          .map { |list| list.reject(&:empty?).map(&:to_i) }
-          .map { |list| new(list) }
+        GroupedList.new(raw_text_list).map { |list| new(list) }
       end
     end
 
     def initialize(items)
-      @items = items
+      @items = convert_to_ints(items)
     end
 
     def total
       @items.sum
+    end
+
+    private
+
+    def convert_to_ints(items)
+      return items if items.all? { |item| item.is_a?(Integer) }
+      items.map(&:to_i)
     end
   end
 
