7a2dba73cb17d78f18b35a61355f0dcc5e24e533
Sean Dickinson <contact@seandickinson.dev>
2022-12-04 13:18:47 -0500

    refactor: rearrange class definitions

--- 04.rb
+++ 04.rb
@@ -1,33 +1,5 @@
 module Day04
   class Assignment
-    class Pair
-      class << self
-        def from(line)
-          assignment_1, assignment_2 = line.split(",").map { |t| Assignment.new(t) }
-          new(assignment_1, assignment_2)
-        end
-      end
-
-      attr_reader :assignment_1, :assignment_2
-      def initialize(assignment_1, assignment_2)
-        @assignment_1 = assignment_1
-        @assignment_2 = assignment_2
-      end
-
-      def full_overlap?
-        assignment_1.cover?(assignment_2) || assignment_2.cover?(assignment_1)
-      end
-
-      def any_overlap?
-        assignment_1.overlaps? assignment_2
-      end
-
-      def ==(other)
-        (assignment_1 == other.assignment_1 && assignment_2 == other.assignment_2) ||
-          (assignment_1 == other.assignment_2 && assignment_2 == other.assignment_2)
-      end
-    end
-
     attr_reader :range
     def initialize(text)
       @range = parse_text(text)
@@ -50,6 +22,34 @@ module Day04
     def parse_text(text)
       start, stop = text.split("-").map(&:to_i)
       start..stop
+    end
+  end
+
+  class Assignment::Pair
+    class << self
+      def from(line)
+        assignment_1, assignment_2 = line.split(",").map { |t| Assignment.new(t) }
+        new(assignment_1, assignment_2)
+      end
+    end
+
+    attr_reader :assignment_1, :assignment_2
+    def initialize(assignment_1, assignment_2)
+      @assignment_1 = assignment_1
+      @assignment_2 = assignment_2
+    end
+
+    def full_overlap?
+      assignment_1.cover?(assignment_2) || assignment_2.cover?(assignment_1)
+    end
+
+    def any_overlap?
+      assignment_1.overlaps? assignment_2
+    end
+
+    def ==(other)
+      (assignment_1 == other.assignment_1 && assignment_2 == other.assignment_2) ||
+        (assignment_1 == other.assignment_2 && assignment_2 == other.assignment_2)
     end
   end
 
