7065a1fda31bf9b47440753e98f859eb181f9462
Sean Dickinson <contact@seandickinson.dev>
2022-12-05 18:45:48 -0500

    feat: scaffold for day 5

---
+++ spec/05_spec.rb
@@ -0,0 +1,14 @@
+require "./05"
+describe Day05 do
+  describe "#part_one" do
+    it "?" do
+      # input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+    end
+  end
+
+  describe "#part_two" do
+    it "?" do
+      # input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+    end
+  end
+end
