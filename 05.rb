5d07a79e400951d76c64db051266e2a913d7c026
Sean Dickinson <contact@seandickinson.dev>
2022-12-06 08:01:16 -0500

    feat: solution to day 5

--- 05.rb
+++ 05.rb
@@ -1,11 +1,150 @@
 module Day05
+  class Stack
+    attr_reader :list
+    def initialize(list)
+      @list = list
+    end
+
+    def push(other)
+      list.unshift(other.list).flatten!
+    end
+
+    def peek
+      list.first
+    end
+
+    def pop(n = 1)
+      self.class.new(list.shift(n))
+    end
+
+    def to_s
+      list.join("\n")
+    end
+
+    def ==(other)
+      other.list == list
+    end
+  end
+
+  class Stack::Group
+    attr_reader :mapping
+    def initialize(mapping)
+      @mapping = mapping
+    end
+
+    def stacks
+      mapping.values
+    end
+
+    def get(key)
+      mapping[key]
+    end
+
+    def move(n, source, dest)
+      dest_stack = get(dest)
+      source_stack = get(source)
+      n.times do
+        dest_stack.push(source_stack.pop)
+      end
+    end
+  end
+
+  class Stack::Group2 < Stack::Group
+    def move(n, source, dest)
+      dest_stack = get(dest)
+      source_stack = get(source)
+      dest_stack.push(source_stack.pop(n))
+    end
+  end
+
+  class Stack::Parser
+    attr_reader :input
+    def initialize(input)
+      @input = input
+    end
+
+    def to_h
+      mapping = empty_column_mapping
+
+      remaining_lines.each do |line|
+        column_to_index_map.each do |column, index|
+          next if line[index] == " "
+          stack = Stack.new([line[index]])
+          mapping[column].push(stack)
+        end
+      end
+
+      mapping
+    end
+
+    def empty_column_mapping
+      column_to_index_map.transform_values { |v| Stack.new([]) }
+    end
+
+    def reversed_input
+      input.reverse_each
+    end
+
+    def first_line
+      reversed_input.first
+    end
+
+    def remaining_lines
+      reversed_input.drop(1)
+    end
+
+    def column_to_index_map
+      map = first_line.each_char.map(&:to_i).map.with_index.to_h
+      map.delete(0)
+      map
+    end
+  end
+
+  class MoveParser
+    def call(input)
+      input.split.map(&:to_i).reject(&:zero?)
+    end
+  end
+
   class << self
     def part_one(input)
-      raise NotImplementedError
+      stacks_input, moves_input = split_input(input)
+      mapping = Stack::Parser.new(stacks_input).to_h
+
+      group = Stack::Group.new(mapping)
+      move_parser = MoveParser.new
+
+      moves_input.each_with_index do |line, index|
+        group.move(*move_parser.call(line))
+      end
+
+      group.stacks.map(&:peek).join
     end
 
     def part_two(input)
-      raise NotImplementedError
+      stacks_input, moves_input = split_input(input)
+      mapping = Stack::Parser.new(stacks_input).to_h
+
+      group = Stack::Group2.new(mapping)
+      move_parser = MoveParser.new
+
+      moves_input.each_with_index do |line, index|
+        group.move(*move_parser.call(line))
+      end
+
+      group.stacks.map(&:peek).join
+    end
+
+    private
+
+    def split_input(input)
+      stacks = input[0...blank_line_index(input)]
+      moves = input[(blank_line_index(input) + 1)..]
+      [stacks, moves]
+    end
+
+    def blank_line_index(input)
+      input.find_index { |line| line == "" }
     end
   end
 end
--- spec/05_spec.rb
+++ spec/05_spec.rb
@@ -1,14 +1,143 @@
 require "./05"
 describe Day05 do
+  describe "Stack" do
+    describe "#to_s" do
+      it "outputs the stack as a string" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+        expected_output = "A\nB\nC"
+        expect(stack.to_s).to eq(expected_output)
+      end
+    end
+
+    describe "#push" do
+      it "pushes the items onto the current stack" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+        other_stack = Day05::Stack.new(["Y", "Z"])
+
+        expected_stack = Day05::Stack.new(["Y", "Z", "A", "B", "C"])
+
+        stack.push(other_stack)
+
+        expect(stack).to eq(expected_stack)
+      end
+    end
+
+    describe "#pop" do
+      it "removes an item from the top of the stack" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+        expected_stack = Day05::Stack.new(["B", "C"])
+
+        stack.pop
+        expect(stack).to eq(expected_stack)
+      end
+
+      it "removes n number of items from the top of the stack" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+        expected_stack = Day05::Stack.new(["C"])
+
+        stack.pop(2)
+        expect(stack).to eq(expected_stack)
+      end
+
+      it "returns the items removed as a new stack" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+        expected_output = Day05::Stack.new(["A"])
+
+        expect(stack.pop).to eq(expected_output)
+      end
+    end
+
+    describe "#peek" do
+      it "returns the top item from the stack without changing the stack" do
+        stack = Day05::Stack.new(["A", "B", "C"])
+
+        expect(stack.peek).to eq("A")
+        expect(stack).to eq(Day05::Stack.new(["A", "B", "C"]))
+      end
+    end
+  end
+
+  describe "Stack::Parser" do
+    describe "#to_h" do
+      it "creates a hash of column names to stacks from the input" do
+        input = File.readlines("spec/test_inputs/05/stacks_only.txt", chomp: true)
+        parser = Day05::Stack::Parser.new(input)
+
+        expected_hash = {
+          1 => Day05::Stack.new(["N", "Z"]),
+          2 => Day05::Stack.new(["D", "C", "M"]),
+          3 => Day05::Stack.new(["P"])
+        }
+
+        expect(parser.to_h).to eq(expected_hash)
+      end
+    end
+  end
+
+  describe "Stack::Group" do
+    describe "#get" do
+      it "returns the stack at the given key" do
+        mapping = {
+          1 => Day05::Stack.new(["A", "B"]),
+          2 => Day05::Stack.new(["C", "D"])
+        }
+        group = Day05::Stack::Group.new(mapping)
+
+        expect(group.get(1)).to eq(mapping[1])
+      end
+    end
+
+    describe "#move" do
+      it "moves 1 of the crates from one stack to another" do
+        mapping = {
+          1 => Day05::Stack.new(["A", "B"]),
+          2 => Day05::Stack.new(["C", "D"])
+        }
+        group = Day05::Stack::Group.new(mapping)
+
+        group.move(1, 1, 2)
+
+        expect(group.get(1)).to eq(Day05::Stack.new(["B"]))
+        expect(group.get(2)).to eq(Day05::Stack.new(["A", "C", "D"]))
+      end
+
+      it "moves 3  crates from one stack to another" do
+        mapping = {
+          1 => Day05::Stack.new(["A", "B", "C"]),
+          2 => Day05::Stack.new(["D"])
+        }
+        group = Day05::Stack::Group.new(mapping)
+
+        group.move(3, 1, 2)
+
+        expect(group.get(1)).to eq(Day05::Stack.new([]))
+        expect(group.get(2)).to eq(Day05::Stack.new(["C", "B", "A", "D"]))
+      end
+    end
+  end
+
+  describe "MoveParser" do
+    describe "#call" do
+      it "parses the move command into an array of the 3 args" do
+        input = "move 1 from 2 to 1"
+        parser = Day05::MoveParser.new
+
+        expect(parser.call(input)).to eq([1, 2, 1])
+      end
+    end
+  end
+
   describe "#part_one" do
-    it "?" do
-      # input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+    it "calculates the crates at the top of each stack" do
+      input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+      expect(Day05.part_one(input)).to eq("CMZ")
     end
   end
 
   describe "#part_two" do
-    it "?" do
-      # input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+    it "calculates the crates at the top of each stack" do
+      input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+      expect(Day05.part_two(input)).to eq("MCD")
     end
   end
 end
