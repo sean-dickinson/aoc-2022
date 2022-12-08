8e64a50f670996d5996008eb91e6a3ac6fcccdc4
Sean Dickinson <contact@seandickinson.dev>
2022-12-08 16:35:50 -0500

    feat: scaffold

---
+++ spec/07_spec.rb
@@ -0,0 +1,15 @@
+require "./07"
+describe Day07 do
+  
+  describe "#part_one" do
+    it "?" do
+     skip
+    end
+  end
+
+  describe "#part_two" do
+    it "?" do
+      skip
+    end
+  end
+end
