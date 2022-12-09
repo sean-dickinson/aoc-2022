2b988d1e6c34c814c44f012b53eacb3c4d55b673
Sean Dickinson <contact@seandickinson.dev>
2022-12-09 08:14:49 -0500

    feat: day 7 solution

--- 07.rb
+++ 07.rb
@@ -1,11 +1,128 @@
 module Day07
+  class File
+    attr_reader :name, :size
+    attr_accessor :parent
+    def initialize(input)
+      @name = extract_name(input)
+      @size = extract_size(input)
+    end
+
+    private
+
+    def extract_name(input)
+      input.split.last
+    end
+
+    def extract_size(input)
+      input.split.first.to_i
+    end
+  end
+
+  class Directory
+    attr_reader :name, :contents
+    attr_accessor :parent
+    def initialize(name)
+      @name = name
+      @contents = []
+    end
+
+    def add(file_or_folder)
+      return if already_contains?(file_or_folder)
+      contents << file_or_folder
+      file_or_folder.parent = self
+    end
+
+    def size
+      contents.sum(&:size)
+    end
+
+    def get_dir(name)
+      contents.find do |file_or_folder|
+        file_or_folder.is_a?(Directory) && file_or_folder.name == name
+      end
+    end
+
+    private
+
+    def already_contains?(file_or_folder)
+      contents.any? do |f|
+        f.name == file_or_folder.name && f.instance_of?(file_or_folder.class)
+      end
+    end
+  end
+
+  class Filesystem
+    class << self
+      def from(input)
+        root_directory = Day07::Directory.new("/")
+        filesystem = new(root_directory)
+        input.each do |line|
+          next if line.start_with? "$ ls"
+          filesystem.parse(line)
+        end
+        filesystem
+      end
+    end
+
+    attr_reader :cwd, :root_dir, :all_directories
+    def initialize(root_dir, cwd = nil)
+      @root_dir = root_dir
+      @cwd = cwd || root_dir
+      @all_directories = []
+      @all_directories << cwd unless cwd.nil?
+    end
+
+    def parse(line)
+      if line.start_with? "$"
+        cd(line.split.last)
+      else
+        add(line)
+      end
+    end
+
+    def cd(dir_name)
+      @cwd = special_values[dir_name] || cwd.get_dir(dir_name)
+    end
+
+    def add(file_or_folder)
+      if file_or_folder.start_with? "dir"
+        folder = Directory.new(file_or_folder.split.last)
+        cwd.add(folder)
+        all_directories << folder
+      else
+        cwd.add(File.new(file_or_folder))
+      end
+    end
+
+    private
+
+    def special_values
+      {
+        ".." => cwd.parent,
+        "/" => root_dir
+      }
+    end
+  end
+
   class << self
+    TOTAL_SPACE = 70000000
+    SPACE_NEEDED = 30000000
     def part_one(input)
-      raise NotImplementedError
+      filesystem = Filesystem.from(input)
+
+      filesystem.all_directories
+        .filter { |dir| dir.size <= 100000 }
+        .sum(&:size)
     end
 
     def part_two(input)
-      raise NotImplementedError
+      filesystem = Filesystem.from(input)
+      space_used = filesystem.root_dir.size
+      current_free_space = TOTAL_SPACE - space_used
+      filesystem.all_directories
+        .filter { |dir| (current_free_space + dir.size) >= SPACE_NEEDED }
+        .min_by(&:size)
+        .size
     end
   end
 end
--- spec/07_spec.rb
+++ spec/07_spec.rb
@@ -1,15 +1,154 @@
 require "./07"
 describe Day07 do
-  
+  describe "File" do
+    describe "#new" do
+      it "creates a file with name and size from the input" do
+        input = "29116 f"
+        file = Day07::File.new(input)
+        expect(file.name).to eq("f")
+        expect(file.size).to eq(29116)
+      end
+    end
+  end
+
+  describe "Directory" do
+    describe "#add" do
+      it "adds a file to the directory contents" do
+        directory = Day07::Directory.new("a")
+        file = Day07::File.new("100 f")
+
+        directory.add(file)
+        expect(directory.contents).to include(file)
+        expect(file.parent).to eq(directory)
+      end
+
+      it "does not add a file if a file with that name already exists" do
+        directory = Day07::Directory.new("a")
+        file = Day07::File.new("100 f")
+        same_name_file = Day07::File.new("200 f")
+
+        directory.add(file)
+
+        expect { directory.add(same_name_file) }.not_to change { directory.contents.size }
+      end
+
+      it "adds a directory to the directory contents" do
+        directory = Day07::Directory.new("a")
+        inner_directory = Day07::Directory.new("b")
+
+        directory.add(inner_directory)
+        expect(directory.contents).to include(inner_directory)
+        expect(inner_directory.parent).to eq(directory)
+      end
+
+      it "does not add a directory if a directory with that name already exists" do
+        directory = Day07::Directory.new("a")
+        inner_directory = Day07::Directory.new("b")
+
+        directory.add(inner_directory)
+        expect(directory.contents).to include(inner_directory)
+
+        expect { directory.add(Day07::Directory.new("b")) }.not_to change { directory.contents.size }
+      end
+    end
+
+    describe "#size" do
+      it "returns the size of all files inside the directory" do
+        directory = Day07::Directory.new("a")
+        [
+          Day07::File.new("100 b"),
+          Day07::File.new("100 c"),
+          Day07::File.new("200 d")
+        ].each do |file|
+          directory.add(file)
+        end
+
+        expect(directory.size).to eq(400)
+      end
+
+      it "includes size of all directories and files inside" do
+        directory = Day07::Directory.new("a")
+        inner_directory = Day07::Directory.new("b")
+
+        [
+          Day07::File.new("100 c"),
+          Day07::File.new("100 d")
+        ].each do |inner_file|
+          inner_directory.add(inner_file)
+        end
+
+        directory.add(Day07::File.new("100 c"))
+        directory.add(inner_directory)
+
+        expect(directory.size).to eq(300)
+      end
+    end
+
+    describe "#get_dir" do
+      it "returns the directory with the name given from the contents" do
+        directory = Day07::Directory.new("a")
+        inner_directory = Day07::Directory.new("b")
+        directory.add(inner_directory)
+
+        expect(directory.get_dir("b")).to eq(inner_directory)
+      end
+
+      it "returns nil if no directory is found" do
+        directory = Day07::Directory.new("a")
+
+        expect(directory.get_dir("b")).to be_nil
+      end
+    end
+  end
+
+  describe "Filesystem" do
+    describe "#cd" do
+      it "changes the cwd to the folder name given" do
+        root_directory = Day07::Directory.new("/")
+        inner_directory = Day07::Directory.new("a")
+        root_directory.add(inner_directory)
+
+        filesystem = Day07::Filesystem.new(root_directory)
+        filesystem.cd("a")
+
+        expect(filesystem.cwd).to eq(inner_directory)
+      end
+
+      it "changes the cwd to the parent folder when the path is .." do
+        root_directory = Day07::Directory.new("/")
+        inner_directory = Day07::Directory.new("a")
+        root_directory.add(inner_directory)
+
+        filesystem = Day07::Filesystem.new(root_directory, inner_directory)
+        filesystem.cd("..")
+
+        expect(filesystem.cwd).to eq(root_directory)
+      end
+
+      it "changes the cwd to the root folder when the path is /" do
+        root_directory = Day07::Directory.new("/")
+        inner_directory = Day07::Directory.new("a")
+        root_directory.add(inner_directory)
+
+        filesystem = Day07::Filesystem.new(root_directory, inner_directory)
+        filesystem.cd("/")
+
+        expect(filesystem.cwd).to eq(root_directory)
+      end
+    end
+  end
+
   describe "#part_one" do
-    it "?" do
-     skip
+    it "Finds the sum of all directories with a size <= 100000" do
+      input = File.readlines("spec/test_inputs/07.txt", chomp: true)
+      expect(Day07.part_one(input)).to eq(95437)
     end
   end
 
   describe "#part_two" do
-    it "?" do
-      skip
+    it "Finds the size of the smallest directory needed to free up enough space" do
+      input = File.readlines("spec/test_inputs/07.txt", chomp: true)
+      expect(Day07.part_two(input)).to eq(24933642)
     end
   end
 end
