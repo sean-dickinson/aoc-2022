6a3ef468a81560e77ce4070804745875dfd88043
Sean Dickinson <contact@seandickinson.dev>
2022-12-30 14:28:54 -0500

    feat: solution for day 13

--- 13.rb
+++ 13.rb
@@ -1,9 +1,146 @@
+require "./11"
 module Day13
+  class Packet
+    attr_reader :list
+    def initialize(list)
+      @list = list
+    end
+
+    def ==(other)
+      list == other.list
+    end
+
+    def <=>(other)
+      return 0 if self == other
+      if self < other
+        -1
+      else
+        1
+      end
+    end
+
+    def <(other)
+      compare_arrays(list, other.list)
+    end
+
+    private
+
+    def zipped_lists(left, right)
+      filled_list(left, right).zip(right)
+    end
+
+    def filled_list(left, right)
+      filled_list = left.clone
+      size_diff = right.size - left.size
+      if size_diff > 0
+        filled_list.fill(nil, left.size, size_diff)
+      end
+      filled_list
+    end
+
+    def compare(left, right)
+      if left.instance_of?(right.class)
+        compare_same(left, right)
+      else
+        compare_different(left, right)
+      end
+    end
+
+    def compare_same(left, right)
+      if left.instance_of?(Integer)
+        compare_integers(left, right)
+      else
+        compare_arrays(left, right)
+      end
+    end
+
+    def compare_arrays(left_list, right_list)
+      zipped_lists(left_list, right_list).each do |left, right|
+        result = compare(left, right)
+        next unless [true, false].include?(result)
+        return result
+      end
+    end
+
+    def compare_integers(left, right)
+      if left == right
+        nil
+      else
+        left < right
+      end
+    end
+
+    def compare_different(left, right)
+      if left.nil? || right.nil?
+        compare_nil(left, right)
+      else
+        convert_and_compare(left, right)
+      end
+    end
+
+    def convert_and_compare(left, right)
+      if left.instance_of?(Integer)
+        compare_arrays([left], right)
+      else
+        compare_arrays(left, [right])
+      end
+    end
+
+    def compare_nil(left, right)
+      left.nil?
+    end
+  end
+
   class << self
     def part_one(input)
+      pairs(input).map.with_index(1).sum do |pair, index|
+        if pair[0] < pair[1]
+          index
+        else
+          0
+        end
+      end
     end
 
     def part_two(input)
+      # debug(input)
+      all_packets(input)
+        .sort
+        .map
+        .with_index(1)
+        .select { |p, _i| divider_packets.include?(p) }
+        .map(&:last)
+        .inject(1, :*)
+    end
+
+    private
+
+    def pairs(input)
+      Day11::InputChunker.new(input)
+        .chunks
+        .map { |line_1, line_2| [Packet.new(eval(line_1)), Packet.new(eval(line_2))] }
+    end
+
+    def all_packets(input)
+      pairs(input).to_a.flatten + divider_packets
+    end
+
+    def debug(input)
+      File.open("./13_debug.txt", "w") do |f|
+        all_packets(input)
+          .sort
+          .each do |packet|
+            f << packet.list.to_s
+            f << "\n"
+          end
+      end
+    end
+
+    def divider_packets
+      [
+        Packet.new([[2]]),
+        Packet.new([[6]])
+      ]
     end
   end
 end
--- spec/13_spec.rb
+++ spec/13_spec.rb
@@ -1,10 +1,174 @@
 require "./13"
 describe Day13 do
+  describe Day13::Packet do
+    describe "#<" do
+      describe "when comparing integers" do
+        it "returns true if the left integer is less than the right" do
+          packet = Day13::Packet.new([1])
+          other_packet = Day13::Packet.new([2])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns true if the first left integer that is not the same is less than the right" do
+          packet = Day13::Packet.new([1, 1])
+          other_packet = Day13::Packet.new([1, 3])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns false if the left integer is greater than the right" do
+          packet = Day13::Packet.new([5])
+          other_packet = Day13::Packet.new([2])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns false if the first left integer that is not the same is greater than the right" do
+          packet = Day13::Packet.new([2, 5])
+          other_packet = Day13::Packet.new([2, 2])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns true if the left packet is smaller than the right and all values are equal" do
+          packet = Day13::Packet.new([2])
+          other_packet = Day13::Packet.new([2, 2])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns false if the left packet is larger than the right and all values are equal" do
+          packet = Day13::Packet.new([2, 2, 2])
+          other_packet = Day13::Packet.new([2, 2])
+
+          expect(packet).not_to be < other_packet
+        end
+      end
+
+      describe "when comparing lists" do
+        it "returns true if the left list has an item that is lower than the right" do
+          packet = Day13::Packet.new([[1]])
+          other_packet = Day13::Packet.new([[2]])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns true if the first left integer that is not the same is less than the right" do
+          packet = Day13::Packet.new([[1, 1]])
+          other_packet = Day13::Packet.new([[1, 2]])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns true if the left list is smaller than the right and all values are the same" do
+          packet = Day13::Packet.new([[1, 1]])
+          other_packet = Day13::Packet.new([[1, 1, 1]])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns false if the left list is longer than the right and all values are the same" do
+          packet = Day13::Packet.new([[1, 1, 1, 1]])
+          other_packet = Day13::Packet.new([[1, 1, 1]])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns false if the first left integer that is not the same is greater than the right" do
+          packet = Day13::Packet.new([[1, 3]])
+          other_packet = Day13::Packet.new([[1, 2]])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns false if the left list has an item that is higher than the right" do
+          packet = Day13::Packet.new([[2]])
+          other_packet = Day13::Packet.new([[1]])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns true when comparing an empty list to a list with items" do
+          packet = Day13::Packet.new([[]])
+          other_packet = Day13::Packet.new([[1]])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns false when comparing a of empty lists to a list with one empty list" do
+          packet = Day13::Packet.new([[[]]])
+          other_packet = Day13::Packet.new([[]])
+
+          expect(packet).not_to be < other_packet
+        end
+      end
+
+      describe "when comparing integers to lists" do
+        it "returns true if the left integer is smaller than the list item" do
+          packet = Day13::Packet.new([1])
+          other_packet = Day13::Packet.new([[2, 1, 1]])
+
+          expect(packet).to be < other_packet
+        end
+
+        it "returns false if the left integer is larger than the list item" do
+          packet = Day13::Packet.new([3])
+          other_packet = Day13::Packet.new([[2, 1, 1]])
+
+          expect(packet).not_to be < other_packet
+        end
+
+        it "returns true for when mixed types" do
+          packet = Day13::Packet.new([1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
+          other_packet = Day13::Packet.new([[1], 4])
+
+          expect(packet).to be < other_packet
+          expect(other_packet).not_to be < packet
+        end
+
+        it "returns true for deeply nested" do
+          packet = Day13::Packet.new([1, 1, 3, 1, 1])
+          other_packet = Day13::Packet.new([1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
+
+          # expect(packet).to be < other_packet
+          expect(other_packet).not_to be < packet
+        end
+      end
+    end
+
+    describe "#<=>" do
+      it "should correctly sort packets" do
+        ordered_packets = [
+          Day13::Packet.new([1, 1, 3, 1, 1]),
+          Day13::Packet.new([1, 1, 5, 1, 1]),
+          Day13::Packet.new([[1], [2, 3, 4]]),
+          Day13::Packet.new([1, [2, [3, [4, [5, 6, 0]]]], 8, 9]),
+          Day13::Packet.new([[1], 4])
+        ]
+        shuffled_packets = [
+          Day13::Packet.new([[1], [2, 3, 4]]),
+          Day13::Packet.new([[1], 4]),
+          Day13::Packet.new([1, 1, 3, 1, 1]),
+          Day13::Packet.new([1, [2, [3, [4, [5, 6, 0]]]], 8, 9]),
+          Day13::Packet.new([1, 1, 5, 1, 1])
+        ]
+        expect(shuffled_packets.sort).to eq(ordered_packets)
+      end
+    end
+  end
+
   describe "#part_one" do
     it "finds sum of all the indicies of packet pairs that are in the right order" do
-      skip
       input = File.readlines("spec/test_inputs/13.txt", chomp: true)
       expect(Day13.part_one(input)).to eq(13)
+    end
+  end
+
+  describe "#part_two" do
+    it "finds product of the indicies of the divider packets when all packets are in sorted order" do
+      input = File.readlines("spec/test_inputs/13.txt", chomp: true)
+      expect(Day13.part_two(input)).to eq(140)
     end
   end
 end
